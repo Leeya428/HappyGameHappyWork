9996150cd3e79e8df742d51417f4f059bd07b5a8
YaLi <CHYaLi@CNYaLi.local>
2017-03-15 17:10:28 +0800

    the first test

--- start_test.py
+++ start_test.py
@@ -1,7 +1,5 @@
 import atx
 
-d = atx.connect('http://localhost:8100',platform='ios')
-d.start_app('com.netease.onmyoji')
-d(xpath=u"//TextField").set_text("")
-d(xpath=u"//SecureTextField").set_text("11111111")
-elem = d(class_name="Button")[0].click();
+driver = atx.connect('http://localhost:8100',platform='ios')
+
+driver.start_app('com.netease.onmyoji')
