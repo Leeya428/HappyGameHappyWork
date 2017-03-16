57c34b3f9c86735fd57b7a64772b67e911f19512
YaLi <CHYaLi@CNYaLi.local>
2017-03-16 11:34:23 +0800

    增加了更新

--- start_test.py
+++ start_test.py
@@ -1,5 +1,13 @@
+#!/usr/local/bin/ python
+# -*- coding: utf-8 -*-
+
 import atx
 
 driver = atx.connect('http://localhost:8100',platform='ios')
+sid = str(driver.start_app('com.netease.onmyoji'))
+fl = open('session_id_ios', 'wb')
+fl.write(sid[16:52])
+fl.close()
+print("session id 已记录")
 
-driver.start_app('com.netease.onmyoji')
+driver.click_image('confirm.1920x1080.png', timeout=8, safe = True)
